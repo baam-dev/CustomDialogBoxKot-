0dfad4c1d9810cec39bd7c514467819ba9b5c753
Amir Bakhshi <amirmobili7@gmail.com>
2022-01-22 18:45:36 +0100

    layout setup

---
+++ app/src/main/java/dev/baam/customalertdialog/MainActivity.kt
@@ -0,0 +1,19 @@
+package dev.baam.customalertdialog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        floatingBtn.setOnClickListener {
+            Log.i("mas", "Click detected ...")
+        }
+
+    }
+}
