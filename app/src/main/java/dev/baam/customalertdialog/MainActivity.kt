e40c405e5ca15f6a8968c9a83fb5821df29d3325
Amir Bakhshi <amirmobili7@gmail.com>
2022-01-23 16:50:24 +0100

    complete number picker

--- app/src/main/java/dev/baam/customalertdialog/MainActivity.kt
+++ app/src/main/java/dev/baam/customalertdialog/MainActivity.kt
@@ -1,9 +1,13 @@
 package dev.baam.customalertdialog
 
+import android.app.Dialog
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.NumberPicker
 import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.alert_view.*
@@ -11,39 +15,51 @@ import kotlinx.android.synthetic.main.alert_view.*
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         floatingBtn.setOnClickListener {
-            setupPickers()
 
-            // inflate the created layout
-            val dialogView = LayoutInflater.from(this).inflate(R.layout.alert_view, null)
+          val dialog = Dialog(this)
+            dialog.setCancelable(true)
+            dialog.setContentView(R.layout.alert_view)
+            setupPickers(dialog)
 
-            // AlertDialogBuilder
-            val builder = AlertDialog.Builder(this).setView(dialogView).setTitle("Add new item")
-
-            // Show the dialog
-            val alertDialog = builder.show()
-
+            dialog.show()
         }
 
     }
 
-    private fun setupPickers () {
+    private fun setupPickers (dialog: Dialog) {
         val grades = arrayOf("A", "B", "C", "D", "E", "F")
-        val credits = arrayOf("50", "100", "150", "200", "300")
-        gradePicker.minValue = 0
-        gradePicker.maxValue = grades.size - 1
-        gradePicker.displayedValues = grades
+        val credits = arrayOf("100", "150", "200", "300", "50")
 
-        creditPicker.minValue = 0
-        creditPicker.maxValue = credits.size - 1
-        creditPicker.displayedValues = credits
+        dialog.apply {
+            gradePicker.minValue = 0
+            gradePicker.maxValue = grades.size - 1
+            gradePicker.displayedValues = grades
 
-        gradePicker.setOnValueChangedListener { numberPicker, oldValue, newValue -> gradeValue.text = "$newValue" }
-        creditPicker.setOnValueChangedListener { numberPicker, oldValue, newValue -> creditValue.text = "$newValue" }
+            creditPicker.minValue = 0
+            creditPicker.maxValue = credits.size - 1
+            creditPicker.displayedValues = credits
+        }
+
+
+
+        dialog.gradePicker.setOnValueChangedListener { numberPicker, oldValue, newValue -> gradeValue.text = "${grades[newValue]}" }
+        dialog.creditPicker.setOnValueChangedListener { numberPicker, oldValue, newValue -> creditValue.text = "${credits[newValue]}" }
+
+
+        dialog.btnAdd.setOnClickListener {
+            println("Click detected ...")
+        }
+
+        dialog.btnCancel.setOnClickListener {
+            dialog.dismiss()
+        }
     }
+
 }
 
 
